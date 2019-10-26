013dc6e6fdb693253499e4ae427a08e93f704dc2
relubwu <bwu@ucsd.edu>
2019-10-26 15:37:54 -0700

    update error-handling procedure

--- server/app.js
+++ server/app.js
@@ -28,6 +28,7 @@ app.use(require('body-parser').json())
 app.post('/classify', [validator.body('img').exists()], async (req, res) => {
   if (!validator.validationResult(req).isEmpty()) { return res.status(400).end() }
   const { body: { img } } = req
+  console.log(img.length)
   require('axios').post(config.tx_api, querystring.stringify({
     key: config.tx_apikey,
     img
@@ -37,7 +38,11 @@ app.post('/classify', [validator.body('img').exists()], async (req, res) => {
     }
   })
     .then(({ data: { code, newslist } }) => {
-      return code === 200 ? Promise.resolve(newslist) : res.status(code).end()
+      if (code === 200)
+        Promise.resolve(newslist)
+      else
+        throw new Error({ msg: `${Code}: Cannot detect object from given data` });
+      // return code === 200 ? Promise.resolve(newslist) : res.status(code).end()
     })
     .then(list => {
       const primary = list[0]
@@ -53,6 +58,9 @@ app.post('/classify', [validator.body('img').exists()], async (req, res) => {
         tip: info[1].TargetText,
         type: info[2]
       })
+    })
+    .catch(e => {
+      return res.json(e.msg);
     })
 })
 
