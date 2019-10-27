4069bf1a3bbf448ea9bcf9a419a5c8ced81eac20
relubwu <bwu@ucsd.edu>
2019-10-27 01:28:44 -0700

    append webcam & capture

--- client/src/views/Scan.vue
+++ client/src/views/Scan.vue
@@ -1,16 +1,24 @@
 <template lang="html">
   <div class="scan">
-    <web-cam
-        ref="webcam"
-        :device-id="deviceId"
-        width="100%"
-        height="100%"
-        @started="onStarted"
-        @stopped="onStopped"
-        @error="onError"
-        @cameras="onCameras"
-        @camera-change="onCameraChange"
-    />
+    <div class="cam-enclsore">
+      <web-cam
+          ref="webcam"
+          :device-id="deviceId"
+          width="100%"
+          height="100%"
+          @started="onStarted"
+          @stopped="onStopped"
+          @error="onError"
+          @cameras="onCameras"
+          @camera-change="onCameraChange"
+      />
+    </div>
+    <div class="controls">
+      <div class="shutter" @click="onCapture">
+        <img src="@/assets/images/shutter.svg" />
+      </div>
+      <!-- <img src="@/assets/images/shutter.png" @click="onCapture" /> -->
+    </div>
   </div>
 </template>
 
@@ -36,6 +44,9 @@ export default {
     }
   },
   watch: {
+    img: function () {
+      console.log(this.$data.img)
+    },
     camera: function(id) {
       this.deviceId = id;
     },
@@ -83,7 +94,44 @@ export default {
 
 <style lang="scss" scoped>
   .scan {
-    height: $router-view-height
+    @include flexbox();
+    @include flex-direction(column);
+    @include align-items(center);
+    @include justify-content(center);
+    height: $router-view-height;
+    background-color: black;
+    .cam-enclosure {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      z-index: 0;
+    }
+    .controls {
+      @include flexbox();
+      @include flex-direction(column);
+      @include align-items(center);
+      @include justify-content(center);
+      position: absolute;
+      bottom: 0;
+      padding-bottom: 88px;
+      width: 100%;
+      background: green;
+      z-index: 1;
+      .shutter {
+        @include flexbox();
+        @include flex-direction(column);
+        @include align-items(center);
+        @include justify-content(center);
+        width: 5rem;
+        height: 5rem;
+        border: none !important;
+        border-radius: 50%;
+        background: linear-gradient(to right, $primary, $secondary);
+        img {
+          filter: drop-shadow(5px 5px 10px rgba(0, 0, 0, 0.25));
+        }
+      }
+    }
   }
 </style>
 <!-- <template>
